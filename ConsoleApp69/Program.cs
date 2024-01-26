83f57f381e4fc210f57ac362bc9875adcbbd92be
sulton <global-sultonmalik326@gmail.com>
2024-01-26 05:38:27 +0500

    Add project files.

---
+++ ConsoleApp69/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace ConsoleApp69;
+
+
+
+
+
+
+class Program
+{
+     
+    static void Main(string[] args)
+    {
+        var a = new Person("John");
+        var b = new Person("John");
+        
+
+        bool equal = a.Name.Equals(b.Name);
+        Console.WriteLine(equal);
+
+    }
+}
+
+
